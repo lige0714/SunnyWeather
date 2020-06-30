0522e9fd4afbafb30b05ebee1b6614724479e4f2
lige0714 <824913478@qq.com>
2020-06-30 09:14:04 +0800

    新增切换城市和手动刷新天气的功能.

--- app/src/main/java/com/sunnyweather/android/SunnyWeatherApplication.kt
+++ app/src/main/java/com/sunnyweather/android/SunnyWeatherApplication.kt
@@ -6,7 +6,7 @@ import android.content.Context
 
 class SunnyWeatherApplication : Application() {
     companion object{
-        const val TOKEN ="TAkhjf8d1nlSlspN"
+        const val TOKEN ="UttWKTHkGMYQHrpk"
         @SuppressLint("StaticFieldLeak")
         lateinit var context:Context
     }
--- app/src/main/java/com/sunnyweather/android/ui/place/PlaceAdapter.kt
+++ app/src/main/java/com/sunnyweather/android/ui/place/PlaceAdapter.kt
@@ -1,5 +1,7 @@
 package com.sunnyweather.android.ui.place
 
+import android.content.Intent
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,8 +10,11 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import com.sunnyweather.android.R
 import com.sunnyweather.android.logic.model.Place
+import com.sunnyweather.android.ui.weather.WeatherActivity
+import kotlinx.android.synthetic.main.activity_weather.*
+import kotlin.math.log
 
-class PlaceAdapter (private val fragment:Fragment,private val placeList: List<Place>):
+class PlaceAdapter (private val fragment:PlaceFragment,private val placeList: List<Place>):
     RecyclerView.Adapter<PlaceAdapter.ViewHolder>() {
     inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val placeName: TextView = view.findViewById(R.id.placeName)
@@ -19,7 +24,31 @@ class PlaceAdapter (private val fragment:Fragment,private val placeList: List<Pl
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
 
         val view = LayoutInflater.from(parent.context).inflate(R.layout.place_item, parent, false)
-        return ViewHolder(view)
+        val holder=ViewHolder(view)
+        holder.itemView.setOnClickListener {
+            val position=holder.adapterPosition
+            val place=placeList[position]
+            val activity =fragment.activity
+            if(activity is WeatherActivity ){
+                activity.drawerLayout.closeDrawers()
+                activity.viewModel.locationlng=place.location.lng
+                activity.viewModel.locationlat=place.location.lat
+                activity.viewModel.placeName=place.name
+                activity.refreshWeather()
+            }else{
+                val intent= Intent(parent.context,WeatherActivity::class.java).apply{
+                    putExtra("location_lng",place.location.lng)
+                    putExtra("location_lat",place.location.lat)
+                    putExtra("place_name",place.name)
+                }
+                fragment.startActivity(intent)
+                fragment.activity?.finish()
+
+            }
+            fragment.viewModel.savePlace(place)
+            Log.e("place",place.name)
+        }
+        return holder
     }
 
     override fun getItemCount()=placeList.size
